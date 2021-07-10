a92aa3f8e54d286e95fd6971f89cb2b858fe4fd4
Julian Webb <git@julianwebb.ca>
2021-07-09 22:22:07 -0700

    Added more information to script start

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 require('dotenv').config();
-require('./logger')("FeedListener", { id: process.env.log_webhook_id, token: process.env.log_webhook_token});
+const packageInfo = require('./package.json');
+require('./logger')(packageinfo.name, { id: process.env.log_webhook_id, token: process.env.log_webhook_token});
 
 const DatabaseHandler = require('./databaseHandler');
 const Database = new DatabaseHandler("database.sqlite");
 const FeedListener = require("./feedListener");
 
-console.log("Starting");
+console.log(`Starting ${packageInfo} v${packageInfo.version}`);
 FeedListener(Database, { id: process.env.feed_webhook_id, token: process.env.feed_webhook_token });
