aba542b255e92ace15b5fe442d224005b4004691
Julian Webb <git@julianwebb.ca>
2021-07-09 23:11:11 -0700

    Fix missing name property in inital log statement

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 const packageInfo = require('./package.json');
-require('./logger')(packageinfo.name, { id: process.env.log_webhook_id, token: process.env.log_webhook_token});
+require('./logger')(packageInfo.name, { id: process.env.log_webhook_id, token: process.env.log_webhook_token});
 
 const DatabaseHandler = require('./databaseHandler');
 const Database = new DatabaseHandler("database.sqlite");
 const FeedListener = require("./feedListener");
 
-console.log(`Starting ${packageInfo} v${packageInfo.version}`);
+console.log(`Starting ${packageInfo.name} v${packageInfo.version}`);
 FeedListener(Database, { id: process.env.feed_webhook_id, token: process.env.feed_webhook_token });
