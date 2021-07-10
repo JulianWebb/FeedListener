0948d2455c0361ae278b62c8d4895fa1545b4a38
Julian Webb <git@julianwebb.ca>
2021-07-09 22:23:58 -0700

    Fixed Default Interval Added Timeout to start of Interval so the feeds get checked at the top of the hour Added Item Sorting as I found out one of the feeds I am using puts their items in reverse order Why isn't PubDate a default of the specification. :(

--- feedHandler.js
+++ feedHandler.js
@@ -1,9 +1,14 @@
 const EventEmitter = require('events');
 const Parser = require('rss-parser');
 
+function millisecondsTilNextHour() {
+    const now = new Date();
+    const then = new Date(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours() + 1);
+    return then - now;
+}
 
 class FeedHandler extends EventEmitter {
-    constructor(name, source, interval = 360000) {
+    constructor(name, source, interval = 3600000) {
         super();
         if (!name) throw("Name Argument Required");
         this.name = name;
@@ -16,19 +21,29 @@ class FeedHandler extends EventEmitter {
             this.latest = item;
         });
         
-        this.setInterval = setInterval(()=> { 
+        setTimeout(() => {
             this.getNewItems(items => {
                 items.forEach((item) => {
                     this.emit("newItem", item)
                 })
             })
-        }, this.interval);
+
+            this.setInterval = setInterval(()=> { 
+                this.getNewItems(items => {
+                    items.forEach((item) => {
+                        this.emit("newItem", item)
+                    })
+                })
+            }, this.interval);
+        }, millisecondsTilNextHour());
+        
     }
 
     getLatestItem(callback) {
         this.parser.parseURL(this.source, (error, feed) => {
             if (error) return console.error(error);
-            callback(feed.items[0]);
+            let items = feed.items[0].pubDate? this.sortItems(feed.items): feed.items;
+            callback(items[0]);
         })
     }
 
@@ -36,15 +51,31 @@ class FeedHandler extends EventEmitter {
         console.log(`Checking '${this.name}' for new items`)
         this.parser.parseURL(this.source, (error, feed) => {
             if (error) return console.error(this.name + " " + error);
-            if (feed.items[0].title == this.latest.title) return;
+            
+            // Most feeds have the lastest on top... some don't
+            // For some reason pubDate is an optional field per the Specification
+            let items = feed.items[0].pubDate? this.sortItems(feed.items): feed.items;
+            if (items[0].title == this.latest.title) return;
             let newItems = [];
-            for (let item of feed.items) {
+            for (let item of items) {
                 if (item.title == this.latest.title) break;
                 newItems.push(item);
             }
-            this.latest = feed.items[0];
+            this.latest = items[0];
             
             callback(newItems.reverse());
+        })
+    }
+
+    sortItems(items) {
+        return items.sort((itemA, itemB) => {
+            let publishDateA = new Date(itemA.pubDate);
+            let publishDateB = new Date(itemB.pubDate);
+            // Sort works by swapping based on the return
+            // -1 for b,a order
+            // 0+ for a,b order
+            // Why they could just do a boolean like normal I have no idea
+            return -1*(publishDateA > publishDateB)
         })
     }
 }
